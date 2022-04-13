dadb338ec1da71268335999b1fa8cd3022bc6a8a
Vikmah <viktoria.mahrberg@gmail.com>
2022-04-13 09:51:31 +0000

    Deployment commit

--- blog/admin.py
+++ blog/admin.py
@@ -5,20 +5,20 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    
-    prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
+
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    summernote_fields = ('content')
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    search_fields = ['name', 'email', 'body']
-    action = ['approved_comments']
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
